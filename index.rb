77920e7b6f9187c85a55b1025312ac48c74af306
Oohira Suguru <suguru.13579515@gmail.com>
2020-04-12 21:46:28 +0900

    Ruby study

---
+++ index.rb
@@ -0,0 +1,28 @@
+a = 1.to_s
+price_tax = 1.08
+b,c = 7,2
+puts a
+puts b,c
+puts "こんにちわ\nさようなら"
+puts "数字は#{b}です。"
+n = 1
+n += 1
+puts n
+
+# 条件分岐
+country = 'italy'
+gree =
+if country == 'japan'
+  'こんにちわ'
+ elsif country == 'italy'
+  'ciao'
+else
+  '???'
+end
+
+puts gree
+
+point = 7
+day = 1
+point *= 5 if day == 1
+puts point 
