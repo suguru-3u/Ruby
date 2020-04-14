7a27c96a74dba754a9e34c41b2c3eea664aed7e6
Oohira Suguru <suguru.13579515@gmail.com>
2020-04-14 17:03:39 +0900

    [Finish] ProRuby 3

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,16 +1,21 @@
 def fizz_buzz(i)
   if i % 15 == 0
-    puts 'fizz_buzz'
+     'fizz_buzz'
   elsif i % 5 == 0
-    puts 'buzz'
+     'buzz'
   elsif i % 3 == 0
-    puts 'Fizz'
+     'Fizz'
   else
     i.to_s
   end
 end
 
-  puts fizz_buzz(1)
-  puts fizz_buzz(3)
-  puts fizz_buzz(5)
-  puts fizz_buzz(15)
+require 'minitest/autorun'
+
+class Fizz_buzz < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1',fizz_buzz(1)
+    assert_equal '2',fizz_buzz(2)
+    assert_equal 'buzz',fizz_buzz(10)
+  end
+end
